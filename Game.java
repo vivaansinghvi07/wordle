1ab1e5ad4d1c44c508235b47fdf0ec4606f05c32
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-15 11:34:08 -0400

    added colors

---
+++ Game.java
@@ -0,0 +1,13 @@
+public class Game {
+
+    // stores the word
+    private String word;
+
+    // stores all the possible words (dictionary)
+    public boolean[] dict;
+
+    // constructor
+    public Game(String[] words) {
+        
+    }
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner wordScanner = new Scanner("wordlewords.txt");
+
+        
+        Game game = new Game();
+    }
+}
