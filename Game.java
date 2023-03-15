58b41cf9c371790a78ad9ed7f2e60655e2eb0349
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-15 11:50:38 -0400

    created constructor for the game

--- Game.java
+++ Game.java
@@ -8,6 +8,32 @@ public class Game {
 
     // constructor
     public Game(String[] words) {
-        
+        // assigns a random word to be the word that is played with
+        this.word = words[(int) (Math.random() * words.length)];
+
+        // loads the dictionary with words
+        this.dict = new boolean[wordToInt("zzzzz") + 1];
+        for (String word : words) {
+            this.dict[wordToInt(word)] = true;
+        }
+
+    }
+
+    // ASSUMES WORDS ARE 5 CHARACTERS LONG - returns a unique integer for each word
+    public static int wordToInt(String word) {
+        // creates an array of characters
+        char[] characters = word.toLowerCase().toCharArray();
+
+        // creates the integer which will be returned
+        int sum = 0;
+
+        // creates a unique integer for the word
+        for (int i = 0; i < 5; i++) {
+            sum += (characters[i] - 'a') * Math.pow(26, i);
+        }
+
+        // returns the integer
+        return sum;
+
     }
 }
