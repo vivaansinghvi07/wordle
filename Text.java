c12b7c5e24ce01f812e823c90b619bc288174d52
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-15 20:44:37 -0400

    added class for text-based methods

---
+++ Text.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Text {
+    
+    // clears the console
+    public static void clear() {
+        System.out.println("\033[H\033[2J");
+        System.out.flush();
+    }
+}
